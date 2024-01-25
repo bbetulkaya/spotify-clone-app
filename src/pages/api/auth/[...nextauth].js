e2913afc435ef00078d236da01048822ba07c01d
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-01-25 16:04:57 +0300

    feat: login and nextauth callbacks

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,30 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
-import { LOGIN_URL } from "../../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefreshToken(token.refreshToken);
+
+    const { body: refreshToken } = await spotifyApi.refreshAccessToken();
+    console.log("REFRESHED TOKEN IS: ", refreshToken);
+
+    return {
+      ...token,
+      accessToken: refreshToken.access_token,
+      accessTokenExpires: Date.now + refreshToken.expires_in * 1000,
+      refreshToken: refreshToken.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -13,6 +37,43 @@ export const authOptions = {
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // initial sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_in * 1000,
+        };
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("EXISTING ACCESS TOKEN IS VALID");
+        return token;
+      }
+
+      // Access token has expired, so we need to refresh it..
+      console.log("EXISTING ACCESS TOKEN HAS EXPIRED!");
+      return await refreshAccessToken(token);
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user.accessToken = token.accessToken;
+        session.user.refreshToken = token.refreshToken;
+        session.user.username = token.username;
+        session.error = token.error
+      }
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import { getProviders, signIn } from "next-auth/react";
+import React from "react";
 
-const login = () => {
+const login = ({ providers }) => {
   return (
-    <div>
-      this is login page
-    </div>
-  )
-}
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img
+        className="w-52 mb-5"
+        src="http://links.papareact.com/9xl"
+        alt="Spotify Logo"
+      />
 
-export default login
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18d860] text-white px-5 py-3 rounded-full"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
